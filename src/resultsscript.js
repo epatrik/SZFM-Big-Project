aff6c65905c1f5d773a8c8854fd7a76b595db797
Vadon <vadondaniel02@gmail.com>
2023-11-22 15:41:42 +0100

    formatting

--- src/resultsscript.js
+++ src/resultsscript.js
@@ -1,173 +1,173 @@
 const queryString = window.location.pathname;
 const id = parseInt(queryString.split('/').pop()); // Parse questionnaireId as integer
 
-const fetchQuestionnaireData = fetch('/api/questionnaireData/:id').then(response => response.json());
-const fetchAnswersData = fetch('/api/answers/:id').then(response => response.json());
+const fetchQuestionnaireData = fetch(`/api/questionnaireData/${id}`).then(response => response.json());
+const fetchAnswersData = fetch(`/api/answers/${id}`).then(response => response.json());
 
 function createBreakdownTable(breakdown) {
-    const breakdownTable = document.createElement('table');
-    breakdownTable.classList.add('breakdown-table');
-  
-    const headerRow = breakdownTable.insertRow();
-    const questionHeader = headerRow.insertCell();
-    questionHeader.textContent = 'Kérdés';
-    const countHeader = headerRow.insertCell();
-    countHeader.textContent = 'Összegzés';
-  
-    Object.keys(breakdown).forEach(question => {
-      const row = breakdownTable.insertRow();
-      const cellQuestion = row.insertCell();
-      cellQuestion.textContent = question;
-  
-      const cellCount = row.insertCell();
-      const countTable = document.createElement('table');
-  
-      Object.keys(breakdown[question]).forEach(option => {
-        const countRow = countTable.insertRow();
-        const optionCell = countRow.insertCell();
-        optionCell.textContent = option;
-        const countCell = countRow.insertCell();
-        countCell.textContent = breakdown[question][option];
-      });
-  
-      cellCount.appendChild(countTable);
+  const breakdownTable = document.createElement('table');
+  breakdownTable.classList.add('breakdown-table');
+
+  const headerRow = breakdownTable.insertRow();
+  const questionHeader = headerRow.insertCell();
+  questionHeader.textContent = 'Kérdés';
+  const countHeader = headerRow.insertCell();
+  countHeader.textContent = 'Összegzés';
+
+  Object.keys(breakdown).forEach(question => {
+    const row = breakdownTable.insertRow();
+    const cellQuestion = row.insertCell();
+    cellQuestion.textContent = question;
+
+    const cellCount = row.insertCell();
+    const countTable = document.createElement('table');
+
+    Object.keys(breakdown[question]).forEach(option => {
+      const countRow = countTable.insertRow();
+      const optionCell = countRow.insertCell();
+      optionCell.textContent = option;
+      const countCell = countRow.insertCell();
+      countCell.textContent = breakdown[question][option];
     });
-  
-    return breakdownTable;
+
+    cellCount.appendChild(countTable);
+  });
+
+  return breakdownTable;
 }
 
 function createBreakdown(questions, filteredAnswers) {
-    const breakdown = {};
-  
-    questions
-      .filter(question => question.type === 'multipleChoice')
-      .forEach(question => {
-        const options = question.answers;
-        breakdown[question.question] = {};
-  
-        options.forEach(option => {
-          breakdown[question.question][option] = 0;
-        });
-  
-        filteredAnswers.forEach(answer => {
-          const index = question.id;
-          const chosenOption = answer.answers[index];
-          if (chosenOption !== '') {
-            breakdown[question.question][chosenOption]++;
-          }
-        });
+  const breakdown = {};
+
+  questions
+    .filter(question => question.type === 'multipleChoice')
+    .forEach(question => {
+      const options = question.answers;
+      breakdown[question.question] = {};
+
+      options.forEach(option => {
+        breakdown[question.question][option] = 0;
       });
-  
-    return breakdown;
-  }
+
+      filteredAnswers.forEach(answer => {
+        const index = question.id;
+        const chosenOption = answer.answers[index];
+        if (chosenOption !== '') {
+          breakdown[question.question][chosenOption]++;
+        }
+      });
+    });
+
+  return breakdown;
+}
 
 function createResultsTable(questions, filteredAnswers) {
-    const resultsTable = document.createElement('table');
-    resultsTable.classList.add('answers-table');
-  
-    const headerRow = resultsTable.insertRow();
-    questions.forEach(question => {
-      const th = document.createElement('th');
-      th.textContent = question.question;
-      headerRow.appendChild(th);
+  const resultsTable = document.createElement('table');
+  resultsTable.classList.add('answers-table');
+
+  const headerRow = resultsTable.insertRow();
+  questions.forEach(question => {
+    const th = document.createElement('th');
+    th.textContent = question.question;
+    headerRow.appendChild(th);
+  });
+
+  filteredAnswers.forEach(answer => {
+    const answerRow = resultsTable.insertRow();
+    answer.answers.forEach((ans, index) => {
+      const td = answerRow.insertCell();
+      td.textContent = ans;
     });
-  
-    filteredAnswers.forEach(answer => {
-      const answerRow = resultsTable.insertRow();
-      answer.answers.forEach((ans, index) => {
-        const td = answerRow.insertCell();
-        td.textContent = ans;
-      });
-    });
-  
-    return resultsTable;
+  });
+
+  return resultsTable;
 }
 
 function createNumberStatistics(questions, filteredAnswers) {
-    const numberStats = {};
-  
-    function calculateModus(numbers) {
-        const numCount = {};
-        let maxCount = 0;
-        let modus = null;
-  
-        numbers.forEach(num => {
-            numCount[num] = (numCount[num] || 0) + 1;
-            if (numCount[num] > maxCount) {
-                maxCount = numCount[num];
-                modus = num;
-            }
-        });
-  
-        return modus;
-    }
+  const numberStats = {};
 
-    questions
-    .filter(question => question.type === 'numberInput')
-    .forEach(question => {
-        const answers = filteredAnswers.map(answer => answer.answers[question.id]);
-        const cleanAnswers = answers.filter(answer => answer !== '');
+  function calculateModus(numbers) {
+    const numCount = {};
+    let maxCount = 0;
+    let modus = null;
 
-        const stats = {
-            count: cleanAnswers.length,
-            average: calculateAverage(cleanAnswers),
-            median: calculateMedian(cleanAnswers),
-            modus: calculateModus(cleanAnswers),
-        };
-
-        numberStats[question.question] = stats;
+    numbers.forEach(num => {
+      numCount[num] = (numCount[num] || 0) + 1;
+      if (numCount[num] > maxCount) {
+        maxCount = numCount[num];
+        modus = num;
+      }
     });
 
-    return numberStats;
+    return modus;
+  }
+
+  questions
+    .filter(question => question.type === 'numberInput')
+    .forEach(question => {
+      const answers = filteredAnswers.map(answer => answer.answers[question.id]);
+      const cleanAnswers = answers.filter(answer => answer !== '');
+
+      const stats = {
+        count: cleanAnswers.length,
+        average: calculateAverage(cleanAnswers),
+        median: calculateMedian(cleanAnswers),
+        modus: calculateModus(cleanAnswers),
+      };
+
+      numberStats[question.question] = stats;
+    });
+
+  return numberStats;
 }
-  
+
 function calculateAverage(numbers) {
-    const sum = numbers.reduce((acc, num) => acc + parseFloat(num), 0);
-    return sum / numbers.length;
+  const sum = numbers.reduce((acc, num) => acc + parseFloat(num), 0);
+  return sum / numbers.length;
 }
-  
+
 function calculateMedian(numbers) {
-    const sortedNumbers = numbers.map(num => parseFloat(num)).sort((a, b) => a - b);
-    const mid = Math.floor(sortedNumbers.length / 2);
-  
-    if (sortedNumbers.length % 2 === 0) {
-      return (sortedNumbers[mid - 1] + sortedNumbers[mid]) / 2;
-    } else {
-      return sortedNumbers[mid];
-    }
+  const sortedNumbers = numbers.map(num => parseFloat(num)).sort((a, b) => a - b);
+  const mid = Math.floor(sortedNumbers.length / 2);
+
+  if (sortedNumbers.length % 2 === 0) {
+    return (sortedNumbers[mid - 1] + sortedNumbers[mid]) / 2;
+  } else {
+    return sortedNumbers[mid];
+  }
 }
 
 function createNumberStatsTable(numberStats) {
-    const numberStatsTable = document.createElement('table');
-    numberStatsTable.classList.add('number-stats-table');
-  
-    const headerRow = numberStatsTable.insertRow();
-    const questionHeader = headerRow.insertCell();
-    questionHeader.textContent = 'Kérdés';
-    const countHeader = headerRow.insertCell();
-    countHeader.textContent = 'Darab';
-    const averageHeader = headerRow.insertCell();
-    averageHeader.textContent = 'Átlag';
-    const medianHeader = headerRow.insertCell();
-    medianHeader.textContent = 'Medián';
-    const modusHeader = headerRow.insertCell();
-    modusHeader.textContent = 'Módusz';
-  
-    Object.keys(numberStats).forEach(question => {
-      const row = numberStatsTable.insertRow();
-      const cellQuestion = row.insertCell();
-      cellQuestion.textContent = question;
-      const cellCount = row.insertCell();
-      cellCount.textContent = numberStats[question].count;
-      const cellAverage = row.insertCell();
-      cellAverage.textContent = numberStats[question].average.toFixed(2); // Limit average to 2 decimal places
-      const cellMedian = row.insertCell();
-      cellMedian.textContent = numberStats[question].median.toFixed(2); // Limit median to 2 decimal places
-      const cellModus = row.insertCell();
-      cellModus.textContent = numberStats[question].modus.toFixed(2); // Limit modus to 2 decimal places
-    });
-  
-    return numberStatsTable;
+  const numberStatsTable = document.createElement('table');
+  numberStatsTable.classList.add('number-stats-table');
+
+  const headerRow = numberStatsTable.insertRow();
+  const questionHeader = headerRow.insertCell();
+  questionHeader.textContent = 'Kérdés';
+  const countHeader = headerRow.insertCell();
+  countHeader.textContent = 'Darab';
+  const averageHeader = headerRow.insertCell();
+  averageHeader.textContent = 'Átlag';
+  const medianHeader = headerRow.insertCell();
+  medianHeader.textContent = 'Medián';
+  const modusHeader = headerRow.insertCell();
+  modusHeader.textContent = 'Módusz';
+
+  Object.keys(numberStats).forEach(question => {
+    const row = numberStatsTable.insertRow();
+    const cellQuestion = row.insertCell();
+    cellQuestion.textContent = question;
+    const cellCount = row.insertCell();
+    cellCount.textContent = numberStats[question].count;
+    const cellAverage = row.insertCell();
+    cellAverage.textContent = numberStats[question].average.toFixed(2); // Limit average to 2 decimal places
+    const cellMedian = row.insertCell();
+    cellMedian.textContent = numberStats[question].median.toFixed(2); // Limit median to 2 decimal places
+    const cellModus = row.insertCell();
+    cellModus.textContent = numberStats[question].modus.toFixed(2); // Limit modus to 2 decimal places
+  });
+
+  return numberStatsTable;
 }
 
 let breakdown = {};
@@ -175,7 +175,9 @@ let numberStats = {};
 
 Promise.all([fetchQuestionnaireData, fetchAnswersData])
   .then(([questionnairesData, answersData]) => {
-    const questionnaire = questionnairesData.find(questionnaire => questionnaire.id === id);
+    console.log('Questionnaires Data:', questionnairesData);
+
+    const questionnaire = questionnairesData.id === id ? questionnairesData : null;
 
     if (questionnaire) {
       const questions = questionnaire.questions;
